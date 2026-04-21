b8d7124d814b98519b7377d99bc88d8a33a4e7bc
Leont1cov <chuchunovvakov@mail.ru>
2026-04-21 20:38:01 +0300

    style: add draft for "Get the Highlights" section

--- src/components/GetTheHighlights/GetTheHighlights.tsx
+++ src/components/GetTheHighlights/GetTheHighlights.tsx
@@ -1,20 +1,23 @@
 import Link from "next/link";
+import styles from "./GetTheHighlights.module.css"
+import Swiper from "@/src/components/Swiper/Swiper";
 
 export default function GetTheHighlights() {
     return (
-        <section>
+        <section className={styles.getTheHighlights}>
             {/*content*/}
             <div>
-                <h1>Get the highlights.</h1>
-
-                {/*links*/}
-                <div>
-                    <Link href="/">Watch the film</Link>
-                    <Link href="/">Watch the event </Link>
+                {/*text content*/}
+                <div className={styles.textContent}>
+                    <h1 className={styles.title}>Get the highlights.</h1>
+                    {/*links*/}
+                    <div>
+                        <Link href="/">Watch the film</Link>
+                        <Link href="/">Watch the event </Link>
+                    </div>
                 </div>
 
-                {/*swiper*/}
-                <div></div>
+                <Swiper/>
 
                 {/*progress bar*/}
                 <div></div>
