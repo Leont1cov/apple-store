6e1517fa95dd6645edb35b7b92423f0b9378e31c
Leont1cov <chuchunovvakov@mail.ru>
2026-04-21 15:33:19 +0300

    feat(page.tsx): add Hero component in page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
 import Header from "@/src/components/Header/Header";
+import Hero from "@/src/components/Hero/Hero";
 
 export default function Home() {
   return (
-    <Header />
+      <>
+          <Header />
+          <Hero/>
+      </>
   );
 }
