13f569626e1968170f16f7601ad99df72d188cb0
Leont1cov <chuchunovvakov@mail.ru>
2026-04-26 21:41:26 +0300

    refactor: highlightsData moved to a separate file

---
+++ src/types/highlightsData.ts
@@ -0,0 +1,22 @@
+export const highlightsData = [
+    {
+        id: 1,
+        video: "/videos/highlight-first.mp4",
+        text: ["Enter A17 Pro.", "Game-changing chip.", "Groundbreaking performance."]
+    },
+    {
+        id: 2,
+        video: "/videos/hightlight-third.mp4",
+        text: ["Titanium.", "So strong. So light. So Pro."]
+    },
+    {
+        id: 3,
+        video: "/videos/hightlight-sec.mp4",
+        text: ["iPhone 15 Pro Max has the", "longest optical zoom in", "iPhone ever. Far out."]
+    },
+    {
+        id: 4,
+        video: "/videos/hightlight-fourth.mp4",
+        text: ["All-new Action button.", "What will yours do?."]
+    }
+];
